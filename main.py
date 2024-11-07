3b445ebb2ad59d55978147618d9d10c6b3aa1bf6
Michael Linson <67348845+mykylyn@users.noreply.github.com>
2024-11-07 17:03:59 -0500

    new code

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-import pygame
+# Simple pygame program
 
+# Import and initialize the pygame library
+import pygame
 pygame.init()
 
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
 
-screen=pygame.display.set_mode((800,400))
-#setting the tile
-pygame.display.set_caption('Runner')
-
-clock = pygame.time.Clock()
 
 while True:
-    pygame.display.update()
 
-    clock.tick(60)
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
